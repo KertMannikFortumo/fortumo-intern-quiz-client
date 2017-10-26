064c406818a615e64dc36dd41e5a245fa7ed8563
Kert Männik <kert.mannik@fortumo.com>
2017-10-26 08:46:34 +0300

    Fixed a typo.

--- src/main/java/ee/kertmannik/quiz/client/Main.java
+++ src/main/java/ee/kertmannik/quiz/client/Main.java
@@ -5,17 +5,17 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        PlayerGreeting PlaterGreeting = new PlayerGreeting();
+        PlayerGreeting PlayerGreeting = new PlayerGreeting();
         QuestionController questionController = new QuestionController();
 
-        String username = PlaterGreeting.getPlayerName();
-        PlaterGreeting.greetPlayer(username);
+        String username = PlayerGreeting.getPlayerName();
+        PlayerGreeting.greetPlayer(username);
 
         try {
             String question = questionController.displayableQuestion(username);
             System.out.println(question);
         } catch (QuizException exception) {
-            System.out.println(exception);
+            System.out.println(exception.getMessage());
         }
     }
 }
